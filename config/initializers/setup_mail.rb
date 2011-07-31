cf8e4839ef83e297889fb886a2481e68d966c2c0
soeren mothes <soeren@mothes.org>
2011-07-31 14:01:54 +0200

    test3 prod smtp Mail

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-        :address => "smtp.soemo.org",
+        #:address => "smtp.soemo.org",
+        :address => "smtprelaypool.ispgateway.de",
         :domain  => "soemo.org",
-        :port => 25,
+        #:port => 25,
+        :port => 465,
         :user_name => "app_postfach@soemo.org",
         :password => "app_postfach",
         :authentication => :login,
