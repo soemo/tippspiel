007ad0067f3df53b694c3b0ed403e16860e51992
soeren mothes <soeren@mothes.org>
2011-07-31 14:11:10 +0200

    test3 prod smtp Mail- use of sendgrid:free

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,11 +1,8 @@
 ActionMailer::Base.smtp_settings = {
-        #:address => "smtp.soemo.org",
-        :address => "smtprelaypool.ispgateway.de",
-        :domain  => "soemo.org",
-        #:port => 25,
-        :port => 465,
-        :user_name => "app_postfach@soemo.org",
-        :password => "app_postfach",
-        :authentication => :login,
-        :enable_starttls_auto => true
+  :address        => "smtp.sendgrid.net",
+  :port           => "25",
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => ENV['SENDGRID_DOMAIN']
 }
