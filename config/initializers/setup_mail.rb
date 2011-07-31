76c2e127f759c4ee9bd0ae2e97f2db19136ba60a
soeren mothes <soeren@mothes.org>
2011-07-31 13:56:34 +0200

    test2 prod smtp Mail

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,9 @@
 ActionMailer::Base.smtp_settings = {
         :address => "smtp.soemo.org",
+        :domain  => "soemo.org",
         :port => 25,
-        :user_name => "postfach@soemo.org",
-        :password => "harald",
-        :authentication => :login
+        :user_name => "app_postfach@soemo.org",
+        :password => "app_postfach",
+        :authentication => :login,
+        :enable_starttls_auto => true
 }
