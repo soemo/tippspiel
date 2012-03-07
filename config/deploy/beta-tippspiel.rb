3c05719e7b87c6d8a11dc91cea4c19aa67bf252b
soeren mothes <soeren@mothes.org>
2012-03-07 20:23:21 +0100

    Setup Capistrano -  default_environment angegeben

--- config/deploy/beta-tippspiel.rb
+++ config/deploy/beta-tippspiel.rb
@@ -8,3 +8,8 @@ set :application, "beta-tippspiel.soemo.org"
 set :deploy_to, "/var/www/virtual/#{user}/#{application}"
 
 set :customizing_dir, "beta-em2012"
+
+# aktuell nutzt ich ruby 1.8.7 ohne rvm
+set :default_environment, {
+  'PATH' => "$HOME/bin/:/.gem/ruby/1.8/bin:/package/host/localhost/ruby-1.8.7/bin/:$PATH:"
+}
