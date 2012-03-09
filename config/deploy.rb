5a4efc8d6c5699e38052019329c2206187001487
soeren mothes <soeren@mothes.org>
2012-03-09 19:57:37 +0100

    Gemfile.lock mit Windows Gems  und ruby Gems

--- config/deploy.rb
+++ config/deploy.rb
@@ -75,7 +75,7 @@ namespace :deploy do
   desc "bundle install --deployment --without development test"
   task :bundle_install, :roles => [:web] do
     run "cd #{release_path} && bundle install --deployment --without development test"
-    run "gem install mysql2 --version=0.3.10 --install-dir='#{release_path}/vendor/bundle/ruby/1.8' --no-ri --no-rdoc"     # TODO soeren 09.03.12 test
+  #  run "gem install mysql2 --version=0.3.10 --install-dir='#{release_path}/vendor/bundle/ruby/1.8' --no-ri --no-rdoc"     # TODO soeren 09.03.12 test
   end
 end
 after "deploy:finalize_update", "deploy:bundle_install"
