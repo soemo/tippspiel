598c1890cd35a77141e2b8ac387f198328fb4645
soeren mothes <soeren@mothes.org>
2012-05-11 23:37:16 +0200

    Bugfix: wenn noch keine Games da sind

--- app/helpers/help_helper.rb
+++ app/helpers/help_helper.rb
@@ -6,7 +6,7 @@ module HelpHelper
       haml_tag :ul, :class => "hilfe" do
         rounds.each do |round|
           start_date_time, end_date_time = Game.round_start_end_date_time(round)
-          start_date = start_date_time.present? ? l(start_date_time, :format => :only_date) :"-"
+          start_date = start_date_time.present? ? l(start_date_time, :format => :only_date) : "-"
           end_date   = end_date_time.present? ? l(end_date_time, :format => :only_date) : "-"
           haml_tag :li, "vom #{start_date} bis #{end_date}: #{t(round, :scope => "round")}"
         end
