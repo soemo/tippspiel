a465447abb8034dfacb4d7c1cf69516e2915494c
soeren mothes <soeren@mothes.org>
2014-01-04 22:24:15 +0100

    #40 Umbruch der Kommentare

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,11 +3,11 @@ module ApplicationHelper
 
   # angepasste devise Methode
   def custom_devise_error_messages!
-    return "" if resource.errors.empty?
+    return '' if resource.errors.empty?
 
-    messages = resource.errors.full_messages.join("<br/>")
+    messages = resource.errors.full_messages.join('<br/>')
     html = <<-HTML
-      <div id="error_explanation" class="alert alert-error">
+      <div id='error_explanation' class='alert alert-error'>
         #{messages}
       </div>
     HTML
@@ -18,32 +18,32 @@ module ApplicationHelper
   # controllername, path, needs_login
   def main_nav_items
     [
-            ["main", root_path, false],
-            ["tipps", tipps_path, true],
-            ["ranking", ranking_path, true],
-            ["notice", notice_path, true],
-            ["help", help_path, false]
+            ['main', root_path, false],
+            ['tipps', tipps_path, true],
+            ['ranking', ranking_path, true],
+            ['notice', notice_path, true],
+            ['help', help_path, false]
     ]
 
   end
 
-  def icon_with_text(icon_name, text="dummytext", add_on_ccs_class="")
-    icon_name.present? ? "<i class='#{icon_name} #{add_on_ccs_class}'></i>".html_safe + " " + text : text
+  def icon_with_text(icon_name, text='dummytext', add_on_ccs_class='')
+    icon_name.present? ? "<i class='#{icon_name} #{add_on_ccs_class}'></i>".html_safe + ' ' + text : text
   end
 
   def get_title
-    t('tournament_name') + " " + t('app_name')
+    "#{t('tournament_name')} #{t('app_name')}"
   end
 
-  def write_team_with_flag(team_name, flag_icon_path="", spacer=nil)
-    haml_concat image_tag(flag_icon_path, :class=>"team_flag") if flag_icon_path.present?
+  def write_team_with_flag(team_name, flag_icon_path='', spacer=nil)
+    haml_concat image_tag(flag_icon_path, :class=>'team_flag') if flag_icon_path.present?
     haml_concat spacer if spacer.present?
     haml_concat team_name
   end
 
   def default_sidebar_content
     # im Fehlerfall wird keine Sidebar angezeigt
-    unless controller.controller_name == "main" && controller.action_name == "error"
+    unless controller.controller_name == 'main' && controller.action_name == 'error'
       write_sidebar_links
       write_sidebar_notes
       write_sidebar_rss_feed(@rss_title, @last_rss_entries)
@@ -54,10 +54,10 @@ module ApplicationHelper
   def write_sidebar_links
     if current_user.present?
       haml_tag :h5 do
-        haml_concat link_to(t("compare_tipps"), compare_tipps_path)
+        haml_concat link_to(t('compare_tipps'), compare_tipps_path)
       end
       haml_tag :h5 do
-        haml_concat link_to(t("hall_of_fame"), hall_of_fame_path)
+        haml_concat link_to(t('hall_of_fame'), hall_of_fame_path)
       end
       haml_tag :hr
     end
@@ -68,7 +68,7 @@ module ApplicationHelper
     haml_tag :p do
       if SIDEBAR_EXTERN_LINKS.present?
         SIDEBAR_EXTERN_LINKS.each do |link_data|
-          haml_concat link_to(link_data[:title], link_data[:url], :target =>"_blank")
+          haml_concat link_to(link_data[:title], link_data[:url], :target =>'_blank')
           haml_tag :br
         end
       end
@@ -80,31 +80,31 @@ module ApplicationHelper
     if current_user.present?
       notes = Notice.limit(5).all
       if notes.present?
-        haml_tag :h4, t("notice")
+        haml_tag 'h4.wrap_text', t('notice')
         notes.each do |n|
           haml_tag :p do
-            haml_tag "span.notice_user", n.user.name if n.user.present?
+            haml_tag 'span.notice_user', n.user.name if n.user.present?
             haml_tag :br
-            haml_concat html_escape(n.text)
+            haml_tag 'span.wrap_text', html_escape(n.text)
           end
         end
       end
-      haml_concat link_to(t("write_notice"), notice_path)
+      haml_concat link_to(t('write_notice'), notice_path)
       haml_tag :br
       haml_tag :hr
     end
   end
 
   def write_sidebar_rss_feed(title, entries)
-    haml_tag :h4, title if title.present?
+    haml_tag 'h4.wrap_text', title if title.present?
     if entries.present?
       entries.each do |e|
         haml_tag :p do
-          haml_tag "span.rss_date", l(e["date_published"].to_date)
+          haml_tag 'span.rss_date', l(e['date_published'].to_date)
           haml_tag :br
-          haml_concat link_to(e["title"], e["urls"][0], :target => "_blank")
+          haml_concat link_to(e['title'], e['urls'][0], :target => '_blank')
           haml_tag :br
-          haml_concat e["content"]
+          haml_tag 'span.wrap_text', e['content']
         end
       end
     end
@@ -119,7 +119,7 @@ module ApplicationHelper
     if nav_items.present?
       haml_tag 'ul.nav' do
         nav_items.each do |key, path, needs_login|
-          class_name = key == controller.controller_name ? "active" : ""
+          class_name = key == controller.controller_name ? 'active' : ''
           haml_tag "li.#{class_name}" do
             haml_concat link_to t(key), path
           end
@@ -136,16 +136,16 @@ module ApplicationHelper
   def write_user_login_logout_link
     if user_signed_in?
       haml_concat t(:signed_in_hello)
-      haml_concat current_user.firstname + " | "
-      haml_concat link_to(icon_with_text("icon-off", t(:sign_out), "icon-white"), destroy_user_session_path)
+      haml_concat current_user.firstname + ' | '
+      haml_concat link_to(icon_with_text('icon-off', t(:sign_out), 'icon-white'), destroy_user_session_path)
     else
-      haml_concat ""
+      haml_concat ''
     end
   end
 
   def write_user_sign_up_link
     if user_signed_in?
-      haml_concat ""
+      haml_concat ''
     else
       haml_concat link_to(t(:sign_up), new_user_registration_path)
     end
