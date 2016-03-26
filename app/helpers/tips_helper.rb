46b8bead3c21ff13a2b69355e82acb48686e5e51
soeren mothes <soeren@mothes.org>
2016-03-27 00:18:38 +0100

    refactoring move on

--- app/helpers/tips_helper.rb
+++ app/helpers/tips_helper.rb
@@ -1,8 +1,8 @@
 # -*- encoding : utf-8 -*-
 module TipsHelper
 
-  def tip_input(tip_id, attr_name, attr_value)
+  def tip_input(tip_id, attr_name, attr_value, disabled=false)
     text_field_tag("tips[#{tip_id}][#{attr_name}]",
-                   attr_value, maxlength: 2, size: 2, class: 'tip_input')
+                   attr_value, maxlength: 2, size: 2, class: 'tip_input', disabled: disabled)
   end
 end
