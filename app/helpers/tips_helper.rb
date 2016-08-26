829f61c21a933a804ae635021fd4f15356174f1d
soeren mothes <soeren@mothes.org>
2016-08-26 14:33:12 +0200

    #142 tipp input with pattern

--- app/helpers/tips_helper.rb
+++ app/helpers/tips_helper.rb
@@ -3,7 +3,7 @@ module TipsHelper
 
   def tip_input(tip_id, attr_name, attr_value)
     name = "tips[#{tip_id}][#{attr_name}]"
-    number_field_tag(name,
+    text_field_tag(name,
                    attr_value,
                    {maxlength: 2, size: 2, class: 'tip_input', pattern: '[0-9]{1,2}'})
   end
--- spec/helpers/tips_helper_spec.rb
+++ spec/helpers/tips_helper_spec.rb
@@ -5,7 +5,7 @@ describe TipsHelper, :type => :helper do
   context '#write_tip_input' do
 
     it 'returns corrct input html' do
-      expect(helper.tip_input(1, 'team1_goals', 2)).to eq("<input type=\"number\" name=\"tips[1][team1_goals]\" id=\"tips_1_team1_goals\" value=\"2\" maxlength=\"2\" size=\"2\" class=\"tip_input\" pattern=\"[0-9]{1,2}\" />")
+      expect(helper.tip_input(1, 'team1_goals', 2)).to eq("<input type=\"text\" name=\"tips[1][team1_goals]\" id=\"tips_1_team1_goals\" value=\"2\" maxlength=\"2\" size=\"2\" class=\"tip_input\" pattern=\"[0-9]{1,2}\" />")
     end
   end
 
