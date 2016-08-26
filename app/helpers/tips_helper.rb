6d717385dcd2f14591bc56f3b83baf19462972ab
soeren mothes <soeren@mothes.org>
2016-08-26 13:02:55 +0200

    test number keypad

--- app/helpers/tips_helper.rb
+++ app/helpers/tips_helper.rb
@@ -3,6 +3,8 @@ module TipsHelper
 
   def tip_input(tip_id, attr_name, attr_value)
     name = "tips[#{tip_id}][#{attr_name}]"
-    text_field_tag(name, attr_value, maxlength: 2, size: 2, class: 'tip_input')
+    text_field_tag(name,
+                   attr_value,
+                   {maxlength: 2, size: 2, class: 'tip_input', pattern: '[0-9]{1,2}'})
   end
 end
