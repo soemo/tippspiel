68140cdb130b733f5501eb0fadbdce631e9c3bd2
soeren mothes <soeren@mothes.org>
2018-02-03 22:51:34 +0100

    #172 gem updates

--- app/presenters/help_presenter.rb
+++ app/presenters/help_presenter.rb
@@ -10,7 +10,11 @@ class HelpPresenter
         start_date_time, end_date_time = Tournament.round_start_end_date_time(round)
         start_date = start_date_time.present? ? I18n.l(start_date_time, format: :only_date) : '-'
         end_date   = end_date_time.present? ? I18n.l(end_date_time, format: :only_date) : '-'
-        result << "#{start_date} - #{end_date}: #{I18n.t(round, scope: 'round')}"
+        if end_date == start_date
+          result << "#{end_date}: #{I18n.t(round, scope: 'round')}"
+        else
+          result << "#{start_date} - #{end_date}: #{I18n.t(round, scope: 'round')}"
+        end
       end
 
     result
--- spec/presenters/help_presenter_spec.rb
+++ spec/presenters/help_presenter_spec.rb
@@ -14,7 +14,6 @@ describe HelpPresenter do
       two_week_ago = DateTime.now - 2.week
       one_week_ago = DateTime.now - 1.week
       four_days_ago = DateTime.now - 4.days
-      three_days_ago = DateTime.now - 3.days
       two_days_ago = DateTime.now - 2.days
 
       expect(Tournament).to receive(:round_start_end_date_time).
@@ -22,12 +21,12 @@ describe HelpPresenter do
       expect(Tournament).to receive(:round_start_end_date_time).
         with(ROUND_OF_16).ordered.and_return([one_week_ago, four_days_ago])
       expect(Tournament).to receive(:round_start_end_date_time).
-        with(FINAL).ordered.and_return([three_days_ago, two_days_ago])
+        with(FINAL).ordered.and_return([two_days_ago, two_days_ago])
 
       expected = [
         "#{I18n.l(one_month_ago, format: :only_date)} - #{I18n.l(two_week_ago, format: :only_date)}: Gruppe",
         "#{I18n.l(one_week_ago, format: :only_date)} - #{I18n.l(four_days_ago, format: :only_date)}: Achtelfinale",
-        "#{I18n.l(three_days_ago, format: :only_date)} - #{I18n.l(two_days_ago, format: :only_date)}: Finale",
+        "#{I18n.l(two_days_ago, format: :only_date)}: Finale",
       ]
       expect(subject.round_infos).to eq(expected)
     end
