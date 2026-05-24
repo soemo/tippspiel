ec0e18c415337f31ae13b7f1b51bbcaa3f9b7a59
Sören Mothes <soeren@mothes.org>
2026-05-24 09:17:30 +0200

    stay on current page after language switch (#135)

--- spec/controllers/locales_controller_spec.rb
+++ spec/controllers/locales_controller_spec.rb
@@ -5,24 +5,23 @@ require 'rails_helper'
 describe LocalesController do
   let(:current_user) { create(:active_user) }
 
-  before { login current_user }
+  # HTTP_REFERER must be a full URL; Rails controller specs use 'http://test.host' as default host.
+  def referer(path)
+    "http://test.host#{path}"
+  end
 
   describe 'PATCH #update' do
     context 'with a supported locale (en)' do
       it 'stores locale in session' do
+        login current_user
         patch :update, params: { locale: 'en' }
         expect(session[:locale]).to eq('en')
-      end
-
-      it 'redirects back' do
-        request.env['HTTP_REFERER'] = root_path
-        patch :update, params: { locale: 'en' }
-        expect(response).to redirect_to(root_path)
       end
     end
 
     context 'with a supported locale (de)' do
       it 'stores locale in session' do
+        login current_user
         patch :update, params: { locale: 'de' }
         expect(session[:locale]).to eq('de')
       end
@@ -30,8 +29,69 @@ describe LocalesController do
 
     context 'with an unsupported locale' do
       it 'stores de as fallback in session' do
+        login current_user
         patch :update, params: { locale: 'fr' }
         expect(session[:locale]).to eq('de')
+      end
+    end
+
+    context 'redirect behaviour when signed in' do
+      before { login current_user }
+
+      it 'redirects back to a protected page (e.g. /rankings)' do
+        request.env['HTTP_REFERER'] = referer(rankings_path)
+        patch :update, params: { locale: 'en' }
+        expect(response).to redirect_to(referer(rankings_path))
+      end
+
+      it 'redirects back to a protected page (e.g. /comparetips)' do
+        request.env['HTTP_REFERER'] = referer(compare_tips_path)
+        patch :update, params: { locale: 'en' }
+        expect(response).to redirect_to(referer(compare_tips_path))
+      end
+
+      it 'redirects back to root' do
+        request.env['HTTP_REFERER'] = referer(root_path)
+        patch :update, params: { locale: 'en' }
+        expect(response).to redirect_to(referer(root_path))
+      end
+
+      it 'redirects to root when referer is blank' do
+        request.env['HTTP_REFERER'] = nil
+        patch :update, params: { locale: 'en' }
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'redirect behaviour when not signed in' do
+      it 'redirects back to the sign-in page' do
+        request.env['HTTP_REFERER'] = referer(new_user_session_path)
+        patch :update, params: { locale: 'en' }
+        expect(response).to redirect_to(referer(new_user_session_path))
+      end
+
+      it 'redirects back to the imprint page' do
+        request.env['HTTP_REFERER'] = referer(imprint_path)
+        patch :update, params: { locale: 'en' }
+        expect(response).to redirect_to(referer(imprint_path))
+      end
+
+      it 'redirects back to the help page' do
+        request.env['HTTP_REFERER'] = referer(help_path)
+        patch :update, params: { locale: 'en' }
+        expect(response).to redirect_to(referer(help_path))
+      end
+
+      it 'redirects to sign-in when referer is blank' do
+        request.env['HTTP_REFERER'] = nil
+        patch :update, params: { locale: 'en' }
+        expect(response).to redirect_to(new_user_session_path)
+      end
+
+      it 'redirects to sign-in when referer is a protected page' do
+        request.env['HTTP_REFERER'] = referer(rankings_path)
+        patch :update, params: { locale: 'en' }
+        expect(response).to redirect_to(new_user_session_path)
       end
     end
   end
